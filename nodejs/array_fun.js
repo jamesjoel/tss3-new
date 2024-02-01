c54af0c069839b30698d15906abfabb215953a27
jamesjoel <james.joelj@gmail.com>
2024-02-01 18:35:53 +0530

    pre-defined fun

---
+++ nodejs/array_fun.js
@@ -0,0 +1,26 @@
+// let data = ["red", "green", "black", "yellow", "pink", "blue"];
+
+// console.log(data.length);
+
+// console.log(data.indexOf("blueeee"));
+
+// let x = data.join(" ");
+// console.log(x)
+
+// data.push("white");
+// data.unshift("white");
+// data.pop();
+// data.pop();
+// data.shift();
+// data.sort();
+// console.log(data.sort());
+// console.log(data.reverse());
+
+let data = ["red", "green", ["rohit", "vijay"], "black", "yellow", "pink", "blue"];
+console.log(data.flat())
+// let arr = ["rohit", "vijay"];
+// let data2 = data.slice(0, 5);
+// console.log(data2)
+
+// let newarr = arr.concat(data);
+// console.log(newarr)
---
+++ nodejs/temp.js
@@ -0,0 +1,7 @@
+let a = "rohit";
+let b = "vijay";
+
+// let x = "--- "+a+" and "+b+" are best friend";
+
+let x = `----${a} and ${b} are best friend`;
+console.log(x)
