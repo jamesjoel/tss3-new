2b17ea0690459d5a1ab00ba679061e7764426a8c
jamesjoel <james.joelj@gmail.com>
2024-02-26 18:30:03 +0530

    react 1

---
+++ react1/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+    <h1>The Stepping Stone</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dolorum voluptatibus incidunt, amet maxime quaerat provident nihil quae doloremque modi possimus esse nostrum blanditiis nam reiciendis facere fugit at! Nulla.</p>
+    <h2>Hello World</h2>
+  </>
+);
+
