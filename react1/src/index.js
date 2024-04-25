0f0f5916bb0bfe4eaa64dfcc8aa4f813fac770fb
jamesjoel <james.joelj@gmail.com>
2024-04-25 18:12:56 +0530

    use context hook

--- react1/src/index.js
+++ react1/src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Box from './Box';
 import Box2 from './Box2';
 import Box3 from './Box3';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <>
-      <Box />
-      <Box2 />
-      <Box3 />
+      <App />
     </>
   
 );
